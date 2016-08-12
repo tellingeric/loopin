c3c98660f5bd91cb104d829970c7f5e6d32ad343
Wenwei <caowenwei@gmail.com>
2016-08-12 11:29:42 -0400

    add push demo

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 angular.module('LoopIn', [
-  'ionic',
+  'ionic','ionic.service.core',
 
   'ngStorage',
 
@@ -35,6 +35,14 @@ angular.module('LoopIn', [
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
+
+    var push = new Ionic.Push({
+      "debug" : true
+    });
+    push.register(function(token){
+      console.log("My Device token:", token.token);
+      push.saveToken(token);
+    });
   });
 })
 
